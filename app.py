8398a1905fdabf93d672d5eb0c7ea5ed9ae6255e
feixiaohui <feixiaohuijava@163.com>
2019-12-11 10:05:18 +0800

    update

--- app.py
+++ app.py
@@ -1,28 +1,54 @@
 from flask import Flask, request
 import json
 import requests
+from tool import get_yaml_data
+import logging
+
 app = Flask(__name__)
 
 
 @app.route('/api/webhook/one', methods=['POST'])
 def hello_world():
-    print("start hello world")
-    data = json.loads(request.data.decode('utf-8'))
-    print(data)
-    receiver_type = data['receive']
-    labels = data['alerts']['labels']
-    annotations = data['alert']['annotations']
-    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=2ea7dd9f-72c3-46fc-bd4e-e279a510dc3e"
-    payload = {
-    "msgtype": "markdown",
-    "markdown": {
-        "content": "实时新增用户反馈<font color='warning'>132例</font>，请相关同事注意。\n>类型:<font color=\"comment\">用户反馈</font> \n>普通用户反馈:<font color=\"comment\">117例</font> \n>VIP用户反馈:<font color=\"comment\">15例</font>"
-    }
-}
-    headers = {"Content-Type": "application/json"}
-    requests.post(url=url, data=json.dumps(payload), headers=headers)
+    if request.data:
+        data = json.loads(request.data.decode('utf-8'))
+        print(f"flask request data: {data}")
+        receiver = data['receiver']
+        status = data['status']
+        alerts = data['alerts']
+        yaml_data = get_yaml_data()
+        alertnames_key = yaml_data['alertnames_key']
+
+        labels_key = yaml_data['labels_key']
+        status_key = yaml_data['status_key']
+        webhook_url_key = yaml_data['webhook_url_key']
+
+        alertname = alerts[0]["labels"]["alertname"]
+        alertnames_zh_key = alertnames_key[alertname]
+        wechat_heard = "监控指标名<font color='warning'>" + alertnames_zh_key + "</font>\n>"
+        wechat_label = ""
+        wechat_status = ""
+        content = wechat_heard
+        for each_alert in alerts:
+            labels = each_alert['labels']
+            annotations = each_alert['annotations']
+            for each_labels_key in labels_key:
+                wechat_label.join(each_labels_key + ":<font color='comment'>" + labels[each_labels_key] + "</font> \n>")
+            for each_status_key in status_key:
+                wechat_status.join(
+                    each_status_key + ":<font color='comment'>" + annotations[each_status_key] + "</font> \n>")
+            content = content.join(wechat_label).join(wechat_status)
+        payload = {
+            "msgtype": "markdown",
+            "markdown": {
+                "content": content
+            }
+        }
+        headers = {"Content-Type": "application/json"}
+        requests.post(url=webhook_url_key, data=json.dumps(payload), headers=headers)
     return "success"
 
 
 if __name__ == '__main__':
+    handler = logging.FileHandler('webhook_wechat.log')
+    app.logger.addHandler(handler)
     app.run(debug=False, host='0.0.0.0', port='5000')
---
+++ tool.py
@@ -0,0 +1,25 @@
+import os
+
+import app
+
+import yaml
+
+
+def get_yaml_data():
+    try:
+        yaml_file = os.path.join(app.app.root_path, 'config', 'webhook.yaml')
+        yaml_data = yaml.load(open(yaml_file), Loader=yaml.FullLoader)
+    except Exception as e:
+        print(f"读取webhook.yaml配置文件出错,原因: {str(e)}")
+        return None
+    alertnames_key = yaml_data['webhook']['alertnames']
+    labels_key = yaml_data['webhook']['labels']
+    status_key = yaml_data['webhook']['status']
+    webhook_url_key = yaml_data['webhook']['webhook_url']
+    return {"alertnames_key": alertnames_key, "labels_key": labels_key, "status_key": status_key,
+            "webhook_url_key": webhook_url_key}
+
+
+if __name__ == '__main__':
+    data = get_yaml_data()
+    print(data)
